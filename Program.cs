94fe8fc71c56bcfe4a1027b8c09b5d01c5954739
brakandres <brakandres20@gmail.com>
2021-04-27 07:56:28 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace poo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+
+            Console.WriteLine("ingrese primer valor");
+            numero1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("ingrese segundo valor");
+            numero2 = Convert.ToInt16(Console.ReadLine());
+
+            Resultado(numero1, numero2);
+        }
+
+        static void Resultado(int n1, int n2)//RESULTADO(INT N1, INT N2) = FIRMA
+        {
+
+        }
+    }
+}
