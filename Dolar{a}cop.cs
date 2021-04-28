ab6fbab23a0cc0ade1ff58f88ca24e8c6c1a58cf
Marlon Andres Jaramillo Jaramillo <60995287+brakandres@users.noreply.github.com>
2021-04-28 01:47:30 -0500

    dolar

---
+++ Dolar{a}cop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolares, pesos;
+            Console.Write("dolares:");
+            dolares = double.Parse(Console.ReadLine());
+            pesos = dolares * 3714.25;
+            Console.WriteLine("dolares={0} equivale a {1} pesos", dolares, pesos);
+            Console.ReadKey();
+        }
+    }
+}
