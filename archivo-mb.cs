06eebb9c6d099d237396cc427bdc3a41179d0a39
Marlon Andres Jaramillo Jaramillo <60995287+brakandres@users.noreply.github.com>
2021-04-28 00:50:46 -0500

    mb

---
+++ archivo-mb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CantidadDeArchivosEnMb
+{
+    class CantidadDeArchivosEnMb
+    {
+        static void Main(string[] args)
+        {
+            int GB, MB;
+            Console.Write("Ingresa el valor de GB: ");
+            GB = int.Parse(Console.ReadLine());
+            MB = GB * 1024;
+            Console.WriteLine("Valor de MB: " + MB);
+            Console.WriteLine();
+            Console.Write("Presiona una tecla para terminar . . . ");
+            Console.ReadKey();
+        }
+    }
+}
