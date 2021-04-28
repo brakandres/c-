6fb09fd88840543f580aa257dba9e819eddd42b6
Marlon Andres Jaramillo Jaramillo <60995287+brakandres@users.noreply.github.com>
2021-04-28 00:29:12 -0500

    programa

---
+++ Program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leer_valor_entero_y_calcular_el_cubo_de_valor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+
+            Console.WriteLine("favor ingresar el numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.Write("el cubo del numero " + numero + "es:" + numero * numero * numero);
+            Console.ReadKey();
+        }
+    }
+}
